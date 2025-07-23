b4ce13febb449d5217d19827832a705fcea080e2
jortegaruiz <jortegaruiz9@gmail.com>
2025-07-23 16:18:56 -0300

    setttings

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Hero() {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl md:text-9xl font-bold text-blue">Bakery</h1>
